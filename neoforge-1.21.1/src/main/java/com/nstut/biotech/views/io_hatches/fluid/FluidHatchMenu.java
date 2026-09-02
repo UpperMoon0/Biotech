767b0bfeea5943f67e15d274aece45b749e5de4d
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-03 02:08:38 +0700

    Port fluid hatch menu to NeoForge handlers

--- neoforge-1.21.1/src/main/java/com/nstut/biotech/views/io_hatches/fluid/FluidHatchMenu.java
+++ neoforge-1.21.1/src/main/java/com/nstut/biotech/views/io_hatches/fluid/FluidHatchMenu.java
@@ -10,49 +10,53 @@ import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.items.SlotItemHandler;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.items.IItemHandler;
+import net.neoforged.neoforge.items.SlotItemHandler;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class FluidHatchMenu extends MachineMenu {
     private final FluidHatchBlockEntity BLOCK_ENTITY;
     private final Level LEVEL;
     private FluidStack fluidStack;
-    // Getters
+
     public FluidStack getFluidStack() {
         return fluidStack;
     }
+
     public FluidHatchBlockEntity getFluidHatchBlockEntity() {
         return BLOCK_ENTITY;
     }
-    // Setters
+
     public void setFluidStack(FluidStack fluidStack) {
         this.fluidStack = fluidStack;
     }
-    public FluidHatchMenu(MenuType menu, int pContainerId, Inventory inventory, BlockEntity blockEntity) {
+
+    public FluidHatchMenu(MenuType<?> menu, int pContainerId, Inventory inventory, BlockEntity blockEntity) {
         super(menu, pContainerId);
         this.BLOCK_ENTITY = (FluidHatchBlockEntity) blockEntity;
         LEVEL = inventory.player.level();
         fluidStack = FluidStack.EMPTY;
 
-        blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(h -> {
-            addSlot(new SlotItemHandler(h, 0, 98, 17));
-            addSlot(new SlotItemHandler(h, 1, 98, 53) {
+        IItemHandler handler = BLOCK_ENTITY.getItemCapability(null);
+        if (handler != null) {
+            addSlot(new SlotItemHandler(handler, 0, 98, 17));
+            addSlot(new SlotItemHandler(handler, 1, 98, 53) {
                 @Override
                 public boolean mayPlace(@NotNull ItemStack stack) {
                     return false;
                 }
             });
-            h.getSlots();
-        });
+        }
 
         addInventorySlots(inventory);
     }
+
     @Override
     public ItemStack quickMoveStack(Player pPlayer, int pIndex) {
         return adaptiveQuickMoveStack(pIndex, 2, 1);
     }
+
     @Override
     public boolean stillValid(Player pPlayer) {
         return stillValid(ContainerLevelAccess.create(LEVEL, BLOCK_ENTITY.getBlockPos()), pPlayer, BlockRegistries.FLUID_INPUT_HATCH.get())
