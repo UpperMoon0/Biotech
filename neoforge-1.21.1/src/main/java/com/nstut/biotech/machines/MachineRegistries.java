39a2e704a63305ae1c6d998f7e7357ee1643faff
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-03 01:47:04 +0700

    Port Biotech machine registries to NeoForge

--- neoforge-1.21.1/src/main/java/com/nstut/biotech/machines/MachineRegistries.java
+++ neoforge-1.21.1/src/main/java/com/nstut/biotech/machines/MachineRegistries.java
@@ -25,6 +25,7 @@ import com.nstut.nstutlib.blocks.MachineBlock;
 import com.nstut.nstutlib.blocks.MachineBlockEntity;
 import com.nstut.nstutlib.recipes.ModRecipe;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -32,21 +33,20 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.extensions.IForgeMenuType;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.network.IContainerFactory;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
+import net.neoforged.neoforge.network.IContainerFactory;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.BiFunction;
 
 public final class MachineRegistries {
-    private static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPE_REGISTER = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, Biotech.MOD_ID);
-    private static final DeferredRegister<Item> ITEM_REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, Biotech.MOD_ID);
-    private static final DeferredRegister<MenuType<?>> MENU_REGISTER = DeferredRegister.create(ForgeRegistries.MENU_TYPES, Biotech.MOD_ID);
-    private static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZER_REGISTER = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, Biotech.MOD_ID);
-    private static final DeferredRegister<Block> BLOCK_REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, Biotech.MOD_ID);
+    private static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPE_REGISTER = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, Biotech.MOD_ID);
+    private static final DeferredRegister<Item> ITEM_REGISTER = DeferredRegister.create(Registries.ITEM, Biotech.MOD_ID);
+    private static final DeferredRegister<MenuType<?>> MENU_REGISTER = DeferredRegister.create(Registries.MENU, Biotech.MOD_ID);
+    private static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZER_REGISTER = DeferredRegister.create(Registries.RECIPE_SERIALIZER, Biotech.MOD_ID);
+    private static final DeferredRegister<Block> BLOCK_REGISTER = DeferredRegister.create(Registries.BLOCK, Biotech.MOD_ID);
 
     public static final MachineRegistry<BreedingChamberBlockEntity, BreedingChamberMenu, BreedingChamberRecipe> BREEDING_CHAMBER = register(
             "breeding_chamber", BreedingChamberBlockEntity::new, BreedingChamberMenu::new, BreedingChamberRecipe.SERIALIZER);
@@ -69,14 +69,13 @@ public final class MachineRegistries {
                                       BiFunction<BlockPos, BlockState, T> blockEntityFactory,
                                       IContainerFactory<U> containerFactory,
                                       RecipeSerializer<Y> recipeSerializer) {
-        RegistryObject<Block> block = BLOCK_REGISTER.register(id, () -> new MachineBlock(blockEntityFactory));
-        RegistryObject<BlockEntityType<T>> blockEntity = BLOCK_ENTITY_TYPE_REGISTER.register(
-                id,
-                () -> BlockEntityType.Builder.of(blockEntityFactory::apply, block.get()).build(null));
-        RegistryObject<Item> blockItem = ITEM_REGISTER.register(id, () -> new BlockItem(block.get(), new Item.Properties()));
+        DeferredHolder<Block, Block> block = BLOCK_REGISTER.register(id, () -> new MachineBlock(blockEntityFactory));
+        DeferredHolder<BlockEntityType<?>, BlockEntityType<T>> blockEntity = BLOCK_ENTITY_TYPE_REGISTER.register(
+                id, () -> BlockEntityType.Builder.of(blockEntityFactory::apply, block.get()).build(null));
+        DeferredHolder<Item, Item> blockItem = ITEM_REGISTER.register(id, () -> new BlockItem(block.get(), new Item.Properties()));
         ItemRegistries.ITEM_SET.add(blockItem);
-        RegistryObject<MenuType<U>> menu = MENU_REGISTER.register(id, () -> IForgeMenuType.create(containerFactory));
-        RegistryObject<RecipeSerializer<Y>> recipeSerializerRegistry = RECIPE_SERIALIZER_REGISTER.register(id, () -> recipeSerializer);
+        DeferredHolder<MenuType<?>, MenuType<U>> menu = MENU_REGISTER.register(id, () -> IMenuTypeExtension.create(containerFactory));
+        DeferredHolder<RecipeSerializer<?>, RecipeSerializer<Y>> recipeSerializerRegistry = RECIPE_SERIALIZER_REGISTER.register(id, () -> recipeSerializer);
         return new MachineRegistry<>(id, block, blockEntity, blockItem, menu, recipeSerializerRegistry);
     }
 
@@ -90,10 +89,10 @@ public final class MachineRegistries {
 
     public record MachineRegistry<T extends MachineBlockEntity, U extends MachineMenu, Y extends ModRecipe<Y>>(
             String id,
-            RegistryObject<Block> block,
-            RegistryObject<BlockEntityType<T>> blockEntity,
-            RegistryObject<Item> blockItem,
-            RegistryObject<MenuType<U>> menu,
-            RegistryObject<RecipeSerializer<Y>> recipeSerializer) {
+            DeferredHolder<Block, Block> block,
+            DeferredHolder<BlockEntityType<?>, BlockEntityType<T>> blockEntity,
+            DeferredHolder<Item, Item> blockItem,
+            DeferredHolder<MenuType<?>, MenuType<U>> menu,
+            DeferredHolder<RecipeSerializer<?>, RecipeSerializer<Y>> recipeSerializer) {
     }
 }
