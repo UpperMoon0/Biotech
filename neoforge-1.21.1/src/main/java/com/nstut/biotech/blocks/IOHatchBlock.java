b855bbe1f53d4e6d1a75e9fc362ef8c4629e6e14
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-03 02:36:52 +0700

    Port hatch menu opening to NeoForge 1.21

--- neoforge-1.21.1/src/main/java/com/nstut/biotech/blocks/IOHatchBlock.java
+++ neoforge-1.21.1/src/main/java/com/nstut/biotech/blocks/IOHatchBlock.java
@@ -4,7 +4,6 @@ import com.nstut.biotech.blocks.entites.hatches.*;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -18,96 +17,83 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class IOHatchBlock extends BaseEntityBlock {
     private final int type;
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
     public IOHatchBlock(int type) {
-        super(BlockBehaviour.Properties.copy(Blocks.GRAY_CONCRETE).strength(2f).sound(SoundType.METAL));
+        super(BlockBehaviour.Properties.ofFullCopy(Blocks.GRAY_CONCRETE).strength(2f).sound(SoundType.METAL));
         this.type = type;
-        this.registerDefaultState(this.stateDefinition.any()
-                .setValue(FACING, Direction.NORTH));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
+
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> blockStateBuilder) {
         blockStateBuilder.add(FACING);
     }
+
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
-    public @NotNull RenderShape getRenderShape(BlockState pState) {
+
+    @Override
+    protected @NotNull RenderShape getRenderShape(BlockState state) {
         return RenderShape.MODEL;
     }
 
     @Nullable
     @Override
-    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return switch (type) {
-            case 0 -> new ItemInputHatchBlockEntity(pPos, pState);
-            case 1 -> new ItemOutputHatchBlockEntity(pPos, pState);
-            case 2 -> new FluidInputHatchBlockEntity(pPos, pState);
-            case 3 -> new FluidOutputHatchBlockEntity(pPos, pState);
-            default -> new EnergyInputHatchBlockEntity(pPos, pState);
+            case 0 -> new ItemInputHatchBlockEntity(pos, state);
+            case 1 -> new ItemOutputHatchBlockEntity(pos, state);
+            case 2 -> new FluidInputHatchBlockEntity(pos, state);
+            case 3 -> new FluidOutputHatchBlockEntity(pos, state);
+            default -> new EnergyInputHatchBlockEntity(pos, state);
         };
     }
+
     @Override
-    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
-        if (!pState.is(pNewState.getBlock())) {
-            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
-            if (blockEntity instanceof ItemHatchBlockEntity)
-            {
-                ((ItemHatchBlockEntity) blockEntity).dropItem();
-            }
-            if (blockEntity instanceof FluidHatchBlockEntity)
-            {
-                ((FluidHatchBlockEntity) blockEntity).dropItem();
-            }
-            super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+    protected void affectNeighborsAfterRemoval(BlockState state, net.minecraft.server.level.ServerLevel level, BlockPos pos, boolean movedByPiston) {
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+        if (blockEntity instanceof ItemHatchBlockEntity itemHatch) {
+            itemHatch.dropItem();
+        } else if (blockEntity instanceof FluidHatchBlockEntity fluidHatch) {
+            fluidHatch.dropItem();
         }
+        super.affectNeighborsAfterRemoval(state, level, pos, movedByPiston);
     }
+
     @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        if (!level.isClientSide) {
-            BlockEntity blockEntity = level.getBlockEntity(pos);
-            switch (type) {
-                case 0:
-                    if (blockEntity instanceof ItemInputHatchBlockEntity)
-                        NetworkHooks.openScreen((ServerPlayer) player, (ItemInputHatchBlockEntity) blockEntity, pos);
-                    break;
-                case 1:
-                    if (blockEntity instanceof ItemOutputHatchBlockEntity)
-                        NetworkHooks.openScreen((ServerPlayer) player, (ItemOutputHatchBlockEntity) blockEntity, pos);
-                    break;
-                case 2:
-                    if (blockEntity instanceof FluidInputHatchBlockEntity)
-                        NetworkHooks.openScreen((ServerPlayer) player, (FluidInputHatchBlockEntity) blockEntity, pos);
-                    break;
-                case 3:
-                    if (blockEntity instanceof FluidOutputHatchBlockEntity)
-                        NetworkHooks.openScreen((ServerPlayer) player, (FluidOutputHatchBlockEntity) blockEntity, pos);
-                    break;
-                default:
-                    if (blockEntity instanceof EnergyInputHatchBlockEntity)
-                        NetworkHooks.openScreen((ServerPlayer) player, (EnergyInputHatchBlockEntity) blockEntity, pos);
-                    break;
-            }
-            return InteractionResult.CONSUME;
-        } else
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
+        if (level.isClientSide()) {
             return InteractionResult.SUCCESS;
+        }
+        if (!(player instanceof ServerPlayer serverPlayer)) {
+            return InteractionResult.PASS;
+        }
+
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+        if (blockEntity instanceof CapabilityBlockEntity hatch) {
+            serverPlayer.openMenu(hatch, buffer -> buffer.writeBlockPos(pos));
+            return InteractionResult.CONSUME;
+        }
+        return InteractionResult.PASS;
     }
+
     @Nullable
     @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
-        if (!pLevel.isClientSide)
-        {
-            if (type == 2 || type == 3)
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+        if (!level.isClientSide()) {
+            if (type == 2 || type == 3) {
                 return FluidHatchBlockEntity::serverTick;
-            else if (type == 4)
+            } else if (type == 4) {
                 return EnergyHatchBlockEntity::serverTick;
+            }
         }
         return null;
     }
