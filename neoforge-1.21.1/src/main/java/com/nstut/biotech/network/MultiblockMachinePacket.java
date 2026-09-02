265800b76179615536073239e8894d09f3f20a32
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-03 01:55:01 +0700

    Port shared machine packet codec helpers to NeoForge

--- neoforge-1.21.1/src/main/java/com/nstut/biotech/network/MultiblockMachinePacket.java
+++ neoforge-1.21.1/src/main/java/com/nstut/biotech/network/MultiblockMachinePacket.java
@@ -2,9 +2,9 @@ package com.nstut.biotech.network;
 
 import com.nstut.nstutlib.recipes.ModRecipeData;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 
 public abstract class MultiblockMachinePacket {
-
     protected int energyCapacity;
     protected int energyStored;
     protected int energyConsumeRate;
@@ -13,4 +13,35 @@ public abstract class MultiblockMachinePacket {
     protected boolean isStructureValid;
     protected BlockPos pos;
     protected ModRecipeData recipe;
+
+    protected void readEnergyPrefix(RegistryFriendlyByteBuf buf) {
+        energyCapacity = buf.readInt();
+        energyStored = buf.readInt();
+        energyConsumeRate = buf.readInt();
+        consumedEnergy = buf.readInt();
+        recipeEnergyCost = buf.readInt();
+    }
+
+    protected void writeEnergyPrefix(RegistryFriendlyByteBuf buf) {
+        buf.writeInt(energyCapacity);
+        buf.writeInt(energyStored);
+        buf.writeInt(energyConsumeRate);
+        buf.writeInt(consumedEnergy);
+        buf.writeInt(recipeEnergyCost);
+    }
+
+    protected void readStateSuffix(RegistryFriendlyByteBuf buf) {
+        isStructureValid = buf.readBoolean();
+        pos = buf.readBlockPos();
+        recipe = buf.readBoolean() ? ModRecipeData.fromBuf(buf) : null;
+    }
+
+    protected void writeStateSuffix(RegistryFriendlyByteBuf buf) {
+        buf.writeBoolean(isStructureValid);
+        buf.writeBlockPos(pos);
+        buf.writeBoolean(recipe != null);
+        if (recipe != null) {
+            recipe.writeToBuf(buf);
+        }
+    }
 }
