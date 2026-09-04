35b7b92835e200b92b1c38e189212064368e8ef7
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-04 19:28:51 +0700

    test(26.1.2): align release pin assertions

--- neoforge-26.1.2/src/test/java/com/nstut/biotech/build/CompatibilityTest.java
+++ neoforge-26.1.2/src/test/java/com/nstut/biotech/build/CompatibilityTest.java
@@ -22,9 +22,9 @@ class CompatibilityTest {
 
         assertEquals("26.1.2", properties.getProperty("minecraft_version_26_1_2"));
         assertEquals("[26.1.2,26.2)", properties.getProperty("minecraft_version_range_26_1_2"));
-        assertEquals("26.1.2.76", properties.getProperty("neoforge_version_26_1_2"));
+        assertEquals("26.1.2.99", properties.getProperty("neoforge_version_26_1_2"));
         assertEquals("0.8", properties.getProperty("nstut_lib_version"));
-        assertEquals("c4eb4005abd2d8fb7d20efab625570896aee7099", properties.getProperty("nstut_lib_ref"));
+        assertEquals("87b129d3d7ea13ae8572917cb9dba74157e442aa", properties.getProperty("nstut_lib_ref"));
 
         String metadataTemplate = Files.readString(repositoryRoot.resolve(Path.of("neoforge-26.1.2", "src/main/templates/META-INF/neoforge.mods.toml")));
         assertTrue(metadataTemplate.contains("versionRange = \"[0.8,0.9)\""));
