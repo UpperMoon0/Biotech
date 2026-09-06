aa6343f15af13c0b58f004173676ae1432eadfad
Sunday Vo <60876500+UpperMoon0@users.noreply.github.com>
2026-09-06 21:57:53 +0700

    test(build): update validated NsTut Lib pin

--- neoforge-26.1.2/src/test/java/com/nstut/biotech/build/CompatibilityTest.java
+++ neoforge-26.1.2/src/test/java/com/nstut/biotech/build/CompatibilityTest.java
@@ -24,7 +24,7 @@ class CompatibilityTest {
         assertEquals("[26.1.2,26.2)", properties.getProperty("minecraft_version_range_26_1_2"));
         assertEquals("26.1.2.99", properties.getProperty("neoforge_version_26_1_2"));
         assertEquals("0.8.1", properties.getProperty("nstut_lib_version"));
-        assertEquals("4888325ea8fc599e73d5342b80a621bc3cb091ea", properties.getProperty("nstut_lib_ref"));
+        assertEquals("2c91ceb5be773e0795453c2cf8fe459cb3ae1abf", properties.getProperty("nstut_lib_ref"));
 
         String metadataTemplate = Files.readString(repositoryRoot.resolve(Path.of("neoforge-26.1.2", "src/main/templates/META-INF/neoforge.mods.toml")));
         assertTrue(metadataTemplate.contains("versionRange = \"[0.8.1,0.9)\""));
