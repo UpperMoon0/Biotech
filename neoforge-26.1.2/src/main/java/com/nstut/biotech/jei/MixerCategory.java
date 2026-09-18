03a0afb8fdebf28f558c5e402cbc93fc9c748907
Sunday Vo <60876500+UpperMoon0@users.noreply.github.com>
2026-09-18 20:44:30 +0700

    Use shared JEI output chance rendering

--- neoforge-26.1.2/src/main/java/com/nstut/biotech/jei/MixerCategory.java
+++ neoforge-26.1.2/src/main/java/com/nstut/biotech/jei/MixerCategory.java
@@ -71,5 +71,6 @@ public class MixerCategory implements IRecipeCategory<MixerRecipe> {
     public void draw(MixerRecipe recipe, @NotNull IRecipeSlotsView slots, GuiGraphicsExtractor graphics, double mouseX, double mouseY) {
         arrow.draw(graphics, 82, 14);
         graphics.text(Minecraft.getInstance().font, "Energy: " + recipe.getTotalEnergy() + " FE", 0, 59, 4210752, false);
+        JeiOutputChanceHelper.drawItemChances(graphics, recipe.getItemOutputs(), 130, 9, 1, 18, 18);
     }
 }
